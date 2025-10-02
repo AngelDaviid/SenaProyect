0d368f9764ebc4a58e70e87528c7ff36d4b0c39e
AngelDaviid <angel_dvalenciae@soy.sena.edu.co>
2025-10-01 19:51:58 -0500

    update chat with conversation's management and message management, add migrations to the database

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,30 +6,44 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['dist/**', 'node_modules/**', 'eslint.config.mjs'],
   },
+
+  // Configs base
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+
+  // Opciones de lenguaje
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module', // NestJS usa ES Modules por default
       parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        project: './tsconfig.json', // apunta a tu tsconfig principal
+        tsconfigRootDir: new URL('.', import.meta.url).pathname,
       },
     },
   },
+
+  // Reglas personalizadas
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      // TypeScript
+      '@typescript-eslint/no-explicit-any': 'off', // Nest usa a veces `any`
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unsafe-call': 'off',
+
+      // Prettier
+      'prettier/prettier': ['warn'],
+
+      // NestJS friendly
+      '@typescript-eslint/explicit-module-boundary-types': 'off', // permite no declarar tipos en controllers/services
+      '@typescript-eslint/ban-ts-comment': 'off', // permite @ts-ignore si lo necesitas temporalmente
     },
-  },
+  }
 );
